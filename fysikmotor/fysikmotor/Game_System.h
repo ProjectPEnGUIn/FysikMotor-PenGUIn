ab1b651e62ab1af1963198427771b01436358cf1
ProjectPEnGUIn <t980409erma@edu.tibro.se>
2017-03-13 13:05:02 +0100

    Sync
    Sync/mergepoint

--- fysikmotor/fysikmotor/Game_System.h
+++ fysikmotor/fysikmotor/Game_System.h
@@ -3,8 +3,6 @@
 #ifndef  _GAMESYSTEM_
 #define _GAMESYSTEM_
 
-
-
 #include "Sound_system.h"
 #include "Texture_System.h"
 #include "Font_System.h"
@@ -46,9 +44,7 @@ public:
 		PE physicsEngine;
 		physicsEngine.init(10, 10, 500, 500, 50, 50);
 
-	//	physicsEngine.addEntity(getEntitySquare(Vec2D(6.5, 8), 5, 1, 100, 0.35, 3, 0, Vec2D(0, 0), 35));
 		physicsEngine.addEntity(getEntitySquare(Vec2D(5, 3), 5, 1, 100, 0.35, 3, 0, Vec2D(0, 0), 30));
-	//	physicsEngine.addEntity(getEntitySquare(Vec2D(5.5, 12), 0.5, 0.5, 2, 0.35, 1, 1, Vec2D(0 ,0), 40));
 		physicsEngine.addEntity(getEntitySquare(Vec2D(5, 10), 1, 0.5, 5, 0.35, 2, 1, Vec2D(-1, 0),30));
 	
 		sf::Time logicTime = sf::seconds(1 / 60.f);
@@ -68,7 +64,7 @@ public:
 			if (logicTimer.getElapsedTime() >= logicTime)
 			{
 				
-				//logic; TODO; split up timer in smaller segments if it ets too big
+				//logic; TODO; split up timer in smaller segments if time gets too big
 				physicsEngine.update(logicTimer.restart().asSeconds() );
 			}
 
